e6a91baeeacee8493b532c39953a12287915240f
kratos2377 <shobityadav23@gmail.com>
2024-01-31 23:53:01 +0530

    Started working on chess game

---
+++ src/chess/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+
+const ChessGame = () => {
+    
+    const [tab, setTab] = useState(0);
+  return (
+ <div>Started working on a chess game</div>
+  )
+}
+
+export default ChessGame
