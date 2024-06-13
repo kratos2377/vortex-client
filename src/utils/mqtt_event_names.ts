daf2c06abed2d08002cd6bc87ef8f490032eadb4
kratos2377 <shobityadav23@gmail.com>
2024-06-14 01:34:39 +0530

    Adding new events

--- src/utils/mqtt_event_names.ts
+++ src/utils/mqtt_event_names.ts
@@ -1,12 +1,10 @@
-
-
 export const USER_ONLINE_EVENT: string = "user-online-event";
 export const FRIEND_REQUEST_EVENT: string = "friend-request-event";
 export const GAME_INVITE_EVENT: string = "game-invite-event";
 export const USER_JOINED_ROOM: string = "user-joined-room";
 export const USER_LEFT_ROOM: string = "user-left-room";
-export const GAME_GENERAL_EVENT: string = "user-game-move";
-export const USER_GAME_MOVE: string = "game-general-event";
+export const GAME_GENERAL_EVENT: string = "game-general-event";
+export const USER_GAME_MOVE: string = "user-game-move";
 export const USER_STATUS_EVENT: string = "user-status-event";
 export const MQTT_USER_EVENTS: string = "user_events/";
 export const MQTT_GAME_EVENTS: string = "game_events/";
