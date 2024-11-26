863a6304b273d70cdaa216672bc413b1287a1851
kratos2377 <shobityadav23@gmail.com>
2024-11-26 21:21:43 +0530

    Minor changes

--- src-tauri/src/utils/qr.rs
+++ src-tauri/src/utils/qr.rs
@@ -2,8 +2,8 @@
 use qrcode_generator::QrCodeEcc;
 
 #[tauri::command]
-pub fn generate_qr_for_bet() -> Vec<u8> {
-    let result: Vec<u8> = qrcode_generator::to_png_to_vec("Hello world!", QrCodeEcc::Low, 1024).unwrap();
+pub fn generate_qr_for_bet(game_room_data: String) -> Vec<u8> {
+    let result: Vec<u8> = qrcode_generator::to_png_to_vec(game_room_data, QrCodeEcc::Low, 1024).unwrap();
 
     result
 
